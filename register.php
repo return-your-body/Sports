a6806a82f6012d4ec139d81cd7ff29f02a06aa3a
ChihYu <109534226@stu.ukn.edu.tw>
2024-12-13 21:46:21 +0800

    打掉重練！

--- register.php
+++ register.php
@@ -8,22 +8,23 @@ include "db.php";
 // 從 POST 獲取表單數據
 $帳號 = $_POST["account"]; // 使用者帳號
 $密碼 = $_POST["passwd"]; // 使用者密碼
+$電子郵件 = $_POST["email"]; // 使用者電子郵件
 
 // 驗證表單輸入
-if ($帳號 == "") {
-    echo "<script>
-            alert('帳號未輸入');
-            window.location.href = 'index.html';
-        </script>";
-    exit();
-}
-if ($密碼 == "") {
-    echo "<script>
-            alert('密碼未輸入');
-            window.location.href = 'index.html';
-        </script>";
-    exit();
-}
+// if ($帳號 == "") {
+//     echo "<script>
+//             alert('帳號未輸入');
+//             window.location.href = 'index.html';
+//         </script>";
+//     exit();
+// }
+// if ($密碼 == "") {
+//     echo "<script>
+//             alert('密碼未輸入');
+//             window.location.href = 'index.html';
+//         </script>";
+//     exit();
+// }
 
 // 查詢「使用者」對應的 grade_id
 $SQL查詢使用者等級 = "SELECT grade_id FROM `grade` WHERE grade = '使用者'";
@@ -58,11 +59,26 @@ $SQL插入使用者 = "INSERT INTO `user` (`account`, `password`, `grade_id`)
                    VALUES ('$帳號', '$密碼', '$使用者等級ID')";
 
 if (mysqli_query($link, $SQL插入使用者)) {
-    // 註冊成功
-    echo "<script>
-            alert('註冊成功！請重新登入。');
-            window.location.href = 'index.html';
-        </script>";
+    // 取得剛插入的 user_id
+    $新使用者ID = mysqli_insert_id($link);
+    
+    // 插入對應的電子郵件到 people 資料表
+    $SQL插入人員資料 = "INSERT INTO `people` (`user_id`, `email`) 
+                        VALUES ('$新使用者ID', '$電子郵件')";
+    
+    if (mysqli_query($link, $SQL插入人員資料)) {
+        // 全部操作成功
+        echo "<script>
+                alert('註冊成功！請重新登入。');
+                window.location.href = 'index.html';
+            </script>";
+    } else {
+        // 插入 people 資料失敗
+        echo "<script>
+                alert('註冊失敗(插入 people 資料失敗)，請稍後再試！');
+                window.location.href = 'index.html';
+            </script>" . mysqli_error($link);
+    }
 } else {
     // 註冊失敗
     echo "<script>
