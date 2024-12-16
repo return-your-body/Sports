d6c29b397176de22f2a7e586d7a4e4ccee9731d2
109534204 <109534204@stu.ukn.edu.tw>
2024-12-16 10:37:57 +0800

    1

--- doctor/d_appointment-records.php
+++ doctor/d_appointment-records.php
@@ -230,7 +230,7 @@
                 <td style='border: 1px solid black; padding: 8px;'>{$row['gender']}</td>
                 <td style='border: 1px solid black; padding: 8px;'>{$row['birthday']}</td>
                 <td style='border: 1px solid black; padding: 8px;'>{$row['appointment_date']}</td>
-                <td style='border: 1px solid black; padding: 8px;'>{$row['appointment_time']}</td>
+             
                 <td style='border: 1px solid black; padding: 8px;'>{$row['doctor']}</td>
                 <td style='border: 1px solid black; padding: 8px;'>{$row['note']}</td>
                 <td style='border: 1px solid black; padding: 8px;'>{$row['created_at']}</td>
